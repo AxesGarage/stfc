fbdc9ae47843bfa1fb32e47ed6019973a950fd4c
Ryan Howe <ryanwhowe@gmail.com>
2024-05-20 08:37:40 -0400

    Updating Territory Dump Command

--- symfony/src/Command/Stfc/DumpTerritoryDataCommand.php
+++ symfony/src/Command/Stfc/DumpTerritoryDataCommand.php
@@ -5,6 +5,7 @@ namespace App\Command\Stfc;
 use App\Command\BaseCommand;
 use App\Utils\Stfc\Territory\Data;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,6 +20,7 @@ class DumpTerritoryDataCommand extends BaseCommand {
 
     protected function configure() {
         $this
+            ->addArgument('format', InputArgument::OPTIONAL, 'Output format (csv or json)', 'csv')
             ->addOption('screen', 's',InputOption::VALUE_NONE, 'Only Output Results table to screen')
             ->setHelp(<<<HELP
 The <info>%command.name%</info> command will generate a csv output to stdOut of the Territory Data Array
@@ -37,18 +39,25 @@ HELP
         parent::execute($input, $output);
 
         $screen = $input->getOption('screen');
+        $format = $input->getArgument('format');
+        if(!in_array($format, ['csv', 'json'])){
+            $this->io->error('Unsupported Format Requested');
+            return self::FAILURE;
+        }
         $territories = Data::getTerritoryData();
-
 
         $result = [];
 
-        foreach($territories as $name=>$data){
+        foreach($territories as $name => $data){
+            $defence = new \DateTime($data[Data::KEY_CAPTURE]);
             $result[] = [
-                'Name' => $name,
-                'Level' => strlen($data[Data::KEY_LEVEL]),
-                'Capture' => $data[Data::KEY_CAPTURE],
-                'Particle' => $data[Data::KEY_PARTICLE],
-                'Routes' => implode(', ', Data::getRoutes($name))
+                'name' => $name,
+                'level' => strlen($data[Data::KEY_LEVEL]),
+                'capture' => $data[Data::KEY_CAPTURE],
+                'dow' => $defence->format('N'),
+                'hour' => $defence->format('H'),
+                'particle' => $data[Data::KEY_PARTICLE],
+                'routes' => implode(', ', Data::getRoutes($name))
             ];
         }
         if($screen){
@@ -58,10 +67,23 @@ HELP
             );
         } else {
             $out = fopen('php://output', 'w');
-            foreach($result as $index => $line) {
-                if(0 === $index) fputcsv($out, array_keys($line));
-                fputcsv($out, $line);
+
+            switch ($format){
+                case 'csv':
+                    foreach($result as $index => $line) {
+                        if(0 === $index) fputcsv($out, array_keys($line));
+                        fputcsv($out, $line);
+                    }
+                    break;
+                case 'json':
+                    $json_result = array_reduce($result, function($json, $item) {
+                        $json[$item['name']] = $item;
+                        return $json;
+                    },[]);
+                    fwrite($out, json_encode($json_result, JSON_NUMERIC_CHECK));
+                    break;
             }
+
             fclose($out);
         }
 
